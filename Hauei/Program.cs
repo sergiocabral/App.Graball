b060e69c3e8c98ef1521435f30e31335f6b2d271
Sergio Cabral <sergio@cabral.br.com>
2019-03-23 01:05:13 -0300

    Add project files.

---
+++ Hauei/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hauei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
