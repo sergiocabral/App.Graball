207ddb68dbaca972a627cac78509923f052c7ae4
Sergio Cabral <sergio@cabral.br.com>
2019-03-27 04:36:20 -0300

    WebClientWithCookie

---
+++ Graball/General/Web/NameValueCollectionExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Graball.General.Web
+{
+    /// <summary>
+    /// Extensão para: NameValueCollection
+    /// </summary>
+    public static class NameValueCollectionExtension
+    {
+        /// <summary>
+        /// Retorna o conteúdo como query string.
+        /// </summary>
+        public static string GetQuerystring(this NameValueCollection values)
+        {
+            var result = new StringBuilder();
+            foreach (string key in values)
+            {
+                if (values[key] != null && values[key].Contains(","))
+                {
+                    foreach (var value in values[key].Split(','))
+                    {
+                        if (result.Length > 0) { result.Append("&"); }
+                        result.Append(string.Format("{0}={1}", key, value));
+                    }                        
+                }
+                else
+                {
+                    if (result.Length > 0) { result.Append("&"); }
+                    result.Append(string.Format("{0}={1}", key, values[key]));
+                }                    
+            };
+            return result.ToString();
+        }
+    }
+}
---
+++ Graball/General/Web/WebClientWithCookieLoadResult.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Graball.General.Web
+{
+    /// <summary>
+    /// Conjunto de dados retornados após um carregamento de página.
+    /// </summary>
+    public struct WebClientWithCookieLoadResult
+    {
+        /// <summary>
+        /// Construtor de inicialização.
+        /// </summary>
+        /// <param name="url">Endereço.</param>
+        /// <param name="success">Indica sucesso.</param>
+        /// <param name="html">Código html.</param>
+        /// <param name="cookies">Cookies.</param>
+        public WebClientWithCookieLoadResult(string url, bool success, string html, string cookies) {
+            Url = url;
+            Exception = null;
+            Success = success;
+            Html = html;
+            Cookies = cookies;
+        }
+
+        /// <summary>
+        /// Construtor para indicar falha.
+        /// </summary>
+        /// <param name="exception"></param>
+        public WebClientWithCookieLoadResult(string url, Exception exception)
+        {
+            Url = url;
+            Exception = exception;
+            Success = false;
+            Html = null;
+            Cookies = null;
+        }
+
+        /// <summary>
+        /// Endereço.
+        /// </summary>
+        public object Url { get; }
+
+        /// <summary>
+        /// Informações sobre o erro.
+        /// </summary>
+        public Exception Exception { get; }
+
+        /// <summary>
+        /// Indica sucesso.
+        /// </summary>
+        public bool Success { get; }
+
+        /// <summary>
+        /// Código html.
+        /// </summary>
+        public string Html { get; }
+
+        /// <summary>
+        /// Cookies.
+        /// </summary>
+        public string Cookies { get; }
+    }
+}
